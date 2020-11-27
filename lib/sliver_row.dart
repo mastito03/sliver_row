f5d5ef87d0dffc9944feec7fa638a4177af60954
Benedictus Arya <mr.aladin03@gmail.com>
2020-11-27 16:07:57 +0700

    add alignment

--- lib/sliver_row.dart
+++ lib/sliver_row.dart
@@ -5,13 +5,22 @@ import 'package:flutter/widgets.dart';
 
 class SliverRow extends StatelessWidget {
   final List<SliverRowChildDelegate> children;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
 
-  const SliverRow({Key key, @required this.children}) : super(key: key);
+  const SliverRow({
+    Key key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    @required this.children,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
       child: Row(
+        mainAxisAlignment: this.mainAxisAlignment,
+        crossAxisAlignment: this.crossAxisAlignment,
         children: [
           for (var child in children)
             child.rowBuilder(
